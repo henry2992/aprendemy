a3adf1e8c5f862d6b6cbebaee866dd187cf053b5
henry2992 <henry2992@hotmail.com>
2016-01-15 17:38:30 -0500

    Fix User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :categories
   has_many :sub_categories
   has_many :questions
-  has_many :choices
+ 
 
   def self.from_omniauth(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
