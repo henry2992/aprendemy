cc8bcc2673cb32e38422116cd32385089b86aa49
Innocent <innocent.amadi@andela.com>
2016-01-23 17:42:38 +0100

    changes database to postgres and fix get question bug

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'pg'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -44,7 +45,7 @@ group :development, :test do
   gem "better_errors"
 
   # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
+  # gem 'sqlite3'
 end
 
 group :development do
@@ -56,5 +57,5 @@ group :development do
 end
 
 group :production do
-  gem 'pg'
+  gem 'rails_12factor'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160120004909) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answered_questions", force: :cascade do |t|
     t.boolean  "correct",     default: false
     t.integer  "user_id"
@@ -84,9 +87,9 @@ ActiveRecord::Schema.define(version: 20160120004909) do
     t.integer  "questions_count",        default: 0
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["provider"], name: "index_users_on_provider"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["uid"], name: "index_users_on_uid"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
 end
