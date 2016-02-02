9e6d6f7062449651ed9a8046ac3153bf70196b61
henry2992 <henry2992@hotmail.com>
2016-02-02 10:21:48 -0600

    Fix uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -36,7 +36,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :medium do
-    process :resize_to_fit => [450, 450]
+    process resize_to_limit: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
