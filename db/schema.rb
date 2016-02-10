2e00666f51dc4a6f44f22a4fc61861963837baf3
henry2992 <henry2992@hotmail.com>
2016-02-09 19:36:48 -0600

    Add explanation to live classes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209182947) do
+ActiveRecord::Schema.define(version: 20160210012929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(version: 20160209182947) do
     t.string   "professor"
     t.string   "url"
     t.boolean  "online_now"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.datetime "time"
+    t.text     "explanation"
   end
 
   create_table "questions", force: :cascade do |t|
