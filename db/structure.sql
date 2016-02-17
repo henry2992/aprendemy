d20bf41c71b63503153586c24775f9b8f2fdceeb
henry2992 <henry2992@hotmail.com>
2016-02-17 05:11:43 -0600

    Structure

--- db/structure.sql
+++ db/structure.sql
@@ -451,7 +451,7 @@ CREATE TABLE simulators (
     id integer NOT NULL,
     user_id integer,
     time_left time without time zone DEFAULT '00:00:30'::time without time zone,
-    last_started timestamp without time zone DEFAULT '2016-02-16 18:12:28.577437'::timestamp without time zone,
+    last_started timestamp without time zone DEFAULT '2016-02-17 11:10:33.534551'::timestamp without time zone,
     last_paused timestamp without time zone,
     time_completed timestamp without time zone,
     created_at timestamp without time zone NOT NULL,
