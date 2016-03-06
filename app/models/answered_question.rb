9ed9f1fb97a8ab9c396c458ded13c1d10515cd3f
Innocent <innocent.amadi@andela.com>
2016-03-06 01:42:13 +0100

    remove pry

--- app/models/answered_question.rb
+++ app/models/answered_question.rb
@@ -7,7 +7,6 @@ class AnsweredQuestion < ActiveRecord::Base
 
   def allocate_points
     point_action = PointAction.find_by_action('answer_question')
-    binding.pry
     if self.correct? && point_action
       point = Point.find_or_create_by(owner_id: self.id, owner_type: 'AnsweredQuestion', recipient_id: self.user_id, recipient_type: 'User')
       point.update(point_action_id: point_action.id,points: point_action.points_alloted)
