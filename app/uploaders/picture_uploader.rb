64f1294c50a662a160ef1064987cfb286aaf6981
henry2992 <henry2992@hotmail.com>
2016-03-11 13:01:13 -0600

    Fix folder name

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,7 +16,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   storage :file unless Rails.env == "production"
 
   def public_id
-    return "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{subcategory.name}/#{model.id}" 
+    return "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}" 
   end 
  
 
