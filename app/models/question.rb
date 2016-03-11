4602c5278e99d5a947301d5604877c61a272084d
henry2992 <henry2992@hotmail.com>
2016-03-11 12:13:17 -0600

    Fix questions show

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   has_many :answered_questions
   has_many :simulator_answered_questions
   belongs_to :user
-  belongs_to :sub_category, counter_cache: true, dependent: :destroy
+  belongs_to :sub_category, counter_cache: true
   mount_uploader :picture, PictureUploader
 
 end
