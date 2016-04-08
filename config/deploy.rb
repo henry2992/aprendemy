2a01e255c27b750280250fd65b6053545435ae18
Hegel Trigo <hegeltrigo@gmail.com>
2016-04-07 22:37:23 -0400

    set binstubs to nil

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.4.0'
+lock '3..0'
 
 set :application, 'aprendemy'
 set :repo_url, 'git@github.com:henry2992/aprendemy.git'
@@ -24,9 +24,8 @@ set :deploy_to, '/home/deploy/aprendemy'
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :bundle_binstubs, nil
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
