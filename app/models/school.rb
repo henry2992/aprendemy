5f98434088779bda249a06f54896b4b1451cb85a
Johnmer Bencomo <bjohnmer@gmail.com>
2016-04-20 08:23:00 -0430

    University - School - Career

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,4 +3,7 @@ class School < ActiveRecord::Base
 
 	validates :name, :presence => true, :length => { :minimum => 2 }
 	validates :description, :presence => true, :length => { :minimum => 2 }
+
+	mount_uploader :image, UserUploader
+
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,8 +1,11 @@
 class University < ActiveRecord::Base
 	has_many :careers
-	
+	enum type: ["Universidad","Instituto"]
+
 	validates :name, :presence => true, :length => { :minimum => 2 }
 	validates :province, :presence => true, :length => { :minimum => 2 }
 	validates :city, :presence => true, :length => { :minimum => 2 }
 	validates :type, :presence => true
+
+	mount_uploader :logo, UserUploader
 end
