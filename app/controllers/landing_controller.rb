c26acb1d8352441988a1680702599cad19486723
henry2992 <henry2992@hotmail.com>
2016-04-28 23:00:09 -0500

    Erase conectiion for landing

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,20 @@
 class LandingController < ApplicationController
 
-	before_filter :authenticate_user!
+	#Helper Methods to allow device resources to be used in home page
+	  helper_method :resource_name, :resource, :devise_mapping
+
+
+	  def resource_name
+		    :user
+	  end
+
+	  def resource
+	    @resource ||= User.new
+	  end
+
+	  def devise_mapping
+	    @devise_mapping ||= Devise.mappings[:user]
+	  end
 	
 
 	def descarga
