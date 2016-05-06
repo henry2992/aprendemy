8341dd538fdca75ae58078b27199709df504f93d
Johnmer Bencomo <bjohnmer@gmail.com>
2016-05-06 15:45:42 -0430

    Adding specs to resources

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-  has_one :resources, :as => :material
+  has_one :resource, :as => :material
   validates :title, :presence => true, :length => { :minimum => 2 }
   validates :content, :presence => true, :length => { :minimum => 2 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  has_one :resources, :as => :material
+  has_one :resource, :as => :material
   has_many :questions
   validates :title, :presence => true, :length => { :minimum => 2 }
   validates :description, :presence => true, :length => { :minimum => 2 }
