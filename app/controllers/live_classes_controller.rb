ca3f8dadd92bebb1cb1de6c2e97906872520d338
Johnmer Bencomo <bjohnmer@gmail.com>
2016-05-07 16:23:46 -0430

    Fixing bug while LiveClasses is empty

--- app/controllers/live_classes_controller.rb
+++ app/controllers/live_classes_controller.rb
@@ -1,8 +1,9 @@
 class LiveClassesController < ApplicationController
   
   def index
-  	@enes = LiveClass.find_by_id(1)
-  	@time = Time.parse(@enes.time.to_s).utc.to_i*1000
+  	@enes = LiveClass.find_by_id(1) # Revisar para obtener un parámetro o usar otro filtro, verificar cuando se destruyen las clases en vivo
+    @time = Time.parse(@enes.time.to_s).utc.to_i*1000 if @enes
+    flash[:notice] = "There is any live class programmed"  if !@time
+    redirect_to :back if !@time
   end
-
 end
