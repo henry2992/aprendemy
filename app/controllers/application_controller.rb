8ec043a8aeb10b31a2651df4c020824d8ba01033
Johnmer Bencomo <bjohnmer@gmail.com>
2016-05-09 15:38:55 -0430

    Add course index view for a student - route: /student/courses/:id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :check_license
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   include ApplicationHelper, AnsweredQuestionsHelper
+
+  def render_404
+     render :file => "public/404.html", :status => 404
+  end
 
   def render_js_only
     respond_to do |format|
--- app/controllers/live_classes_controller.rb
+++ app/controllers/live_classes_controller.rb
@@ -3,7 +3,7 @@ class LiveClassesController < ApplicationController
   def index
   	@enes = LiveClass.find_by_id(1) # Revisar para obtener un parámetro o usar otro filtro, verificar cuando se destruyen las clases en vivo
     @time = Time.parse(@enes.time.to_s).utc.to_i*1000 if @enes
-    flash[:notice] = "There is any live class programmed"  if !@time
+    flash[:notice] = "No hay clases en vivo programadas"  if !@time
     redirect_to :back if !@time
   end
 end
