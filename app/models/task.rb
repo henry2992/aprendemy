51586e908c978929d6d32cea952dc82c0b532d1d
Johnmer Bencomo <bjohnmer@gmail.com>
2016-06-16 12:02:29 -0400

    Fix menu options for admin
    Menu Admin has to have Courses link, this already fixed + fixed bug in
    task -> questions

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   has_one :resource, :as => :material
 
   has_many :answers, :as => :item
-  has_many :questions
+  has_many :questions, :as => :parent 
   
   validates :title, :presence => true, :length => { :minimum => 2 }
   validates :description, :presence => true, :length => { :minimum => 2 }
