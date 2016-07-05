4c0af0765e96860a2ba26cd792184c811e697e83
Johnmer Bencomo <bjohnmer@gmail.com>
2016-07-05 16:40:57 -0400

    Minor fix in user menu

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,8 @@
 class Plan < ActiveRecord::Base
 
+  has_many :course_user_plans
+  has_many :course_users, through: :course_user_plans
+  
   enum licence_type: [:free,:paid]
 
   validates_presence_of :name, :expiration_days, :licence_type, :price
