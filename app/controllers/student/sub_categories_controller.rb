60b50a00d1ffc6f730de7b74995d42f5321108a8
henry2992 <henry2992@hotmail.com>
2016-08-01 18:26:02 -0500

    Change Questions Pagination View

--- app/controllers/student/sub_categories_controller.rb
+++ app/controllers/student/sub_categories_controller.rb
@@ -5,8 +5,12 @@ class Student::SubCategoriesController < Student::StudentController
       @sub_category = SubCategory.find_by_id(params[:id])
       page_questions = params[:page] if params[:paginate] == "q"
       page_answered = params[:page] if params[:paginate] == "a"
-      @questions = @sub_category.questions.where(parent_id:nil).where.not(id: Answer.where(user: current_user).map(&:question_id)).paginate(:page => page_questions)
-      @answered = @sub_category.questions.where(parent_id:nil).where(id: Answer.where(user: current_user).map(&:question_id)).paginate(:page => page_answered)
+
+
+      @questions = @sub_category.questions.where(parent_id:nil).where.not(id: Answer.where(user: current_user).map(&:question_id)).paginate(:page => page_questions, :per_page => 1)
+      @answered = @sub_category.questions.where(parent_id:nil).where(id: Answer.where(user: current_user).map(&:question_id)).paginate(:page => page_answered, :per_page => 20)
   end
+
+
   
 end
