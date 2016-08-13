fc0511b8957a90f7c3f242c74bd14d9c1dbf7022
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-13 17:12:46 -0400

    minor fix to test deploy AWS Johnmer

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:henry2992/aprendemy.git' # Edit this to match you
 set :deploy_to, '/home/deploy/aprendemy'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 
 set :rvm_type, :user
