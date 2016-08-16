8c3b172db84d32dbda73578973e2844fc93c1ac0
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-16 18:03:11 -0400

    Adding testing deploy linode johnmer

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,7 +14,7 @@ set :rails_env, 'production'
 # Extended Server Syntax
 # ======================
 server '45.79.97.233', user: 'deploy', roles: %w{web app db} # Linode Johnmer
-server '104.236.228.208', user: 'deploy', roles: %w{web app db} # Digital Ocean Testing server
+# server '104.236.228.208', user: 'deploy', roles: %w{web app db} # Digital Ocean Testing server
 
 # This can be used to drop a more detailed server
 # definition into the server list. The second argument
