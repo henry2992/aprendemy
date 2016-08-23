e7bfc46b57da08d1e7d157c7816147d0cce67e73
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-23 12:44:14 -0400

    Adding facebook sign up

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  provider :facebook, 
-          "834590109976033", 
+          Rails.application.secrets.facebook_app_id, 
           Rails.application.secrets.facebook_secret,  
           scope: 'email, public_profile', info_fields: 'email, first_name, last_name, gender', :image_size => 'large'
 end
