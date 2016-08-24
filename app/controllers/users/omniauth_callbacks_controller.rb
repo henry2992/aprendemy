6e56ae291c4f4929e2e4baf3e55c342c38f47c05
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-24 16:57:20 -0400

    Adding facebook sign up

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    return render json { value: "data data" }
+    render json { value: "data data" }
     # # Implementaremos este método en nuestro modelo más adelante
     # @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -15,7 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    return render json { value: "data data" }
+    render json { value: "data 2" }
 
     # redirect_to root_path
   end
