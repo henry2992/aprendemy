3a2ca549569cebc5184adf1744caa5ebfa11b34e
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-25 11:19:41 -0400

    Adding facebook sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path
+    student_courses_path
   end
 
   def set_current_user
