df4b60d31d3b493c59166ff1780373e9bfd1788b
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-25 15:21:29 -0400

    Adding facebook sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # def after_sign_in_path_for(resource)
+  #   request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  #   # student_courses_path
+  # end
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    # student_courses_path
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || student_courses_path
+    end
   end
 
   def set_current_user
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,11 @@
-class SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
   # skip_before_filter :verify_signed_out_user
   # before_filter :configure_sign_in_params, only: [:create]
-
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    store_location_for(resource, params[:redirect_to])
+    super
+  end
   # GET /resource/sign_in
   # def new
   #   super
