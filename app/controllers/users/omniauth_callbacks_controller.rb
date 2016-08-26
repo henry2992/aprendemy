4b05222920444a182e6994aac129e984fec010e7
Johnmer Bencomo <bjohnmer@gmail.com>
2016-08-26 15:06:03 -0400

    Adding facebook sign up

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # render json: { value: "data data" }
+    puts request.env["omniauth.auth"]
     # Implementaremos este método en nuestro modelo más adelante
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user # this will throw if @user is not activated
+      # render json: { value: request.env["omniauth.auth"] }
+      sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      # return redirect_to student_courses_path
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"].except('extra')
       redirect_to new_user_registration_url
@@ -16,7 +16,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    # render json: { value: request.env["omniauth.auth"] }
     redirect_to root_path
   end
 end
