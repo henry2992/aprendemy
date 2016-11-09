e609d9c13a8942b04450305c686eefe463908ecb
Johnmer Bencomo <bjohnmer@gmail.com>
2016-11-09 14:43:52 -0400

    Reverse order for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all.order(start_time: :desc)
+    @events = Event.all.order(:start_time)
     @events_types = EventType.all
   end
 
