fd80325a3403276033b93d58095e78ed5e26145c
Johnmer Bencomo <bjohnmer@gmail.com>
2016-11-09 14:15:28 -0400

    Reverse order for Events

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,19 +41,19 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Added by Cent. The default url options for user by devise
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     :address              => "smtp.gmail.com",
-  #     :port                 => 587,
-  #     :domain               => "localhost:3000",
-  #     :user_name            => ENV["GMAIL_USERNAME"],
-  #     :password             => ENV["GMAIL_PASSWORD"],
-  #     :authentication       => 'plain',
-  #     :openssl_verify_mode  => 'none',
-  #     :enable_starttls_auto => true
-  #   }
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+       :address              => "smtp.gmail.com",
+       :port                 => 587,
+       :domain               => "localhost:3000",
+       :user_name            => ENV["GMAIL_USERNAME"],
+       :password             => ENV["GMAIL_PASSWORD"],
+       :authentication       => 'plain',
+       :openssl_verify_mode  => 'none',
+       :enable_starttls_auto => true
+     }
   
-  config.action_mailer.delivery_method = :sparkpost
+  # config.action_mailer.delivery_method = :sparkpost
 
 end
