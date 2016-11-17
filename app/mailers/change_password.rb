ecba9f93349a031350e5ad6b24bb50c9e9efb0b0
Johnmer Bencomo <bjohnmer@gmail.com>
2016-11-17 12:38:24 -0400

    Sending Emails

---
+++ app/mailers/change_password.rb
@@ -0,0 +1,20 @@
+class ChangePassword < ApplicationMailer
+  def send_mail (user 
+    sub_data = { 
+                  first_name: user.first_name, 
+                  last_name: user.last_name, 
+                }
+    data = {
+    #   track_opens: true,
+    #   track_clicks: false,
+    #   campaign_id: "My Campaign",
+    #   transactional: true,
+    #   ip_pool = "SPECIAL_POOL",
+    #   api_key = "MESSAGE_SPECIFIC_API_KEY"
+    #   subaccount = "123"
+      substitution_data: sub_data, 
+      template_id: "change-password"
+    }
+    mail(to: user.email, subject: "Hola #{user.first_name}, puedes cambiar tu clave", body: "", sparkpost_data: data)
+  end
+end
