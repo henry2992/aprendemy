e1f5b88e1ab6bd8c55f8a9cf800f33a507240a30
Johnmer Bencomo <bjohnmer@gmail.com>
2017-01-05 10:44:14 -0400

    Improve attitude tests

--- app/controllers/student/attitude_tests_controller.rb
+++ app/controllers/student/attitude_tests_controller.rb
@@ -23,12 +23,19 @@ class Student::AttitudeTestsController < Student::StudentController
                 }
 
     end
+    
+    band = false
+
+    if params['action_form'] == "end" && current_user.user_attitude_tests.find_by_test_id(params[:id]).test.questions.count == vs.count
+      current_user.user_attitude_tests.find_by_test_id(params[:id]).update_attribute(:status,1)
+      band = true
+    end
+
     if Answer.create!(data) and data.any?
       if params['action_form'] == "pause" && current_user.user_attitude_tests.find_by_test_id(params[:id]).test.questions.count >= vs.count
         return redirect_to student_attitude_test_path(params['id']), notice: 'Test pausado correctamente, para finalizar un test debe contestar todas las preguntas' 
       end
-      if params['action_form'] == "end" && current_user.user_attitude_tests.find_by_test_id(params[:id]).test.questions.count == vs.count
-        current_user.user_attitude_tests.find_by_test_id(params[:id]).update_attribute(:status,1)
+      if band
         return redirect_to student_courses_path, notice: 'Hemos recibido tus pruebas de actitud, muy pronto recibiras tu resultados por email' 
       else
         return redirect_to student_courses_path, notice: 'Test pausado, para finalizar un test debe contestar todas las preguntas' 
