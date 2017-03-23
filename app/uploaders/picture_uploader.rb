d8986ffe103f8154718656c474ca36fc6a8589ea
Johnmer Bencomo <bjohnmer@gmail.com>
2017-03-23 18:18:20 -0400

    Fix Cloudinary public_id

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,8 +16,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # storage :file unless Rails.env == "production"
 
   def public_id
-    return model.short_name
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    return "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
 end
