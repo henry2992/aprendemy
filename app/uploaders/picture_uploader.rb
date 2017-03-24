0160b407e4c3a1043ada3863b1769c1ddc3e9a06
Johnmer Bencomo <bjohnmer@gmail.com>
2017-03-23 21:14:16 -0400

    Fix Cloudinary public_id

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -19,6 +19,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   return "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
