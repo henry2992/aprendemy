a3e5269898c06641cee70ea9c734f708c5cea189
Johnmer Bencomo <bjohnmer@gmail.com>
2017-03-31 11:34:04 -0400

    Fix cloudinary public_id

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -17,7 +17,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   def public_id
     # public_id
-    return "#{model.short_name}/#{mounted_as}/#{model.id}"
+    # return "#{model.short_name}/#{mounted_as}/#{model.id}"
+    uniq_id = Digest::MD5.hexdigest("#{Time.now.utc}-#{current_path}-#{rand}")
+    file_id = File.basename(original_filename, '.*')
+    "#{file_id}-#{uniq_id}"  
   end
   # def store_dir
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
