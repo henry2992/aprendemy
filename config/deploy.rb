40a8657df6e138880bfed6cef585f0f969887499
Johnmer Bencomo <bjohnmer@gmail.com>
2017-04-07 21:29:40 -0430

    Edit comment in rake tasks in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :puma_preload_app, false
 
 namespace :rake do  
   desc "Run a task on a remote server."  
-  # run like: cap staging rake:invoke task=a_certain_task  
+  # run like: cap staging invoke:rake task=a_certain_task  
   task :invoke do
     run "cd #{deploy_to}/current"
     run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
