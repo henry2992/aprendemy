2f7ba888ec992c6fef0e73fbfb153d759832b7a4
Johnmer Bencomo <bjohnmer@gmail.com>
2017-04-12 01:42:25 -0400

    handling email delivery errors

--- app/mailers/change_password.rb
+++ app/mailers/change_password.rb
@@ -2,21 +2,6 @@ class ChangePassword < Devise::Mailer
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::UrlHelper
 
-  around_action do |mailer, block|
-    begin
-      block.call
-    rescue SparkPostRails::DeliveryException => e
-      Rails.logger.error "/----------- Error enviando Email al Cambiar Password --------------/"
-      Rails.logger.error "Archivo: app/mailers/change_password.rb"
-      Rails.logger.error "Función: reset_password_instructions"
-      Rails.logger.error "Usuario #: "+ user.id
-      Rails.logger.error "Email: "+ user.email
-      Rails.logger.error "Error: "+ e.message
-      return true
-    end
-  end
-
-
   def reset_password_instructions (user,token, opts={})
     @resource = user
     @token = token
@@ -31,12 +16,13 @@ class ChangePassword < Devise::Mailer
       template_id: "change-password"
     }
     mail(to: @resource.email, subject: "Hola #{@resource.first_name}, puedes cambiar tu clave", body: "", sparkpost_data: data)
-  # rescue => e
-  #   Rails.logger.error "/----------- Error enviando Email al Cambiar Password --------------/"
-  #   Rails.logger.error "Archivo: app/mailers/change_password.rb"
-  #   Rails.logger.error "Función: reset_password_instructions"
-  #   Rails.logger.error "Usuario #: "+ user.id
-  #   Rails.logger.error "Email: "+ user.email
-  #   Rails.logger.error "Error: "+ e.message
+  rescue Exception => e
+    Rails.logger.error "/----------- Error enviando Email al Cambiar Password --------------/"
+    Rails.logger.error "Archivo: app/mailers/change_password.rb"
+    Rails.logger.error "Función: reset_password_instructions"
+    Rails.logger.error "Usuario #: "+ user.id
+    Rails.logger.error "Email: "+ user.email
+    Rails.logger.error "Error: "+ e.message
+    return true
   end
 end
